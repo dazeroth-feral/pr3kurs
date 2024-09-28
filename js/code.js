f2646db359894053d2453d6b91bd2ca6dc43644d
dazeroth <99990841+dazeroth-feral@users.noreply.github.com>
2024-09-29 01:08:12 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,355 @@
+const song_db = {
+    0: {
+        id: 0,
+        name: "JoJo's BA | Op. 6 - chase",
+        creator: "CyberCodeFX",
+        genre: "-",
+        count_listener: "687 тис.",
+        words: `NO HAVE TEXT`,
+        yt_link: "https://music.youtube.com/watch?v=aCmJCQfINVU&list=LM"
+    },
+
+    1: {
+        id: 1,
+        name: "Not Allowed",
+        creator: "TV Girl",
+        genre: "-",
+        count_listener: "67 млн.",
+        words: `
+            Now you suck
+            We wanna talk about sex but we're not allowed
+            Well, you may not like it but you better learn how 'cause it's your turn now
+            You're wasting your tongue with lame excuses and lies
+
+            So how should I begin this?
+            I guess it started when you were with him
+            And how he never even took you out to dance
+            But did he fuck with any rhythm?
+
+            But now he's playing with your head
+            But did he ever make you cum?
+            Did he ever make you cry?
+            Do the wires in your mind get sewn together
+
+            Rubbed and severed by the heat
+            You don't know how long I could stare into your picture
+            And wish that it was me
+            I guess it's different 'cause you love him
+            But I've got an interactive
+            Sick and twisted imagination
+            And that's gotta count for something
+
+            We wanna talk about sex but we're not allowed
+            Well you may not like it but you'd better learn how 'cause it's your turn now
+            You're wasting your tongue with lame excuses and lies
+            Get your face between my thighs
+
+            I dreamt I was standing in your doorstep
+            Licking sweat off of your forehead
+            With your finger in my mouth
+            And the sound when leather jackets hit the ground
+
+            You should hear when you're not around
+            When it's just us horny poets
+            Who can't wait to write it down
+            And swear we were only being honest
+
+            Do you like these little sonnets
+            'Cause I wrote them just for you
+            But how quickly they turn sour
+            So be careful who you screw
+            And never call
+            And I'm starting to suspect
+            You don't intend to do anything you say at all
+
+            We wanna talk about sex but we're not allowed
+            Well you may not like it but you'd better learn how 'cause it's your turn now
+            You're wasting your tongue with lame excuses and lies
+
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind
+            All by yourself, sittin' alone
+            I hope we're still friends, yeah, I hope you don't mind`,
+        yt_link: "https://music.youtube.com/watch?v=r0xTqwKMOyk&list=LM"
+    },
+
+    2: {
+        id: 2,
+        name: "TELL ME WHY (BERSERK)",
+        creator: "PENPALS",
+        genre: "Гітара",
+        count_listener: "2 млн.",
+        words: `
+            Feel no shame about shape
+            Weather changes their phrase
+            Even mother will show you another way
+            So put your glasses on
+
+            Nothing will be wrong
+            There's no blame, there's no fame
+            It's up to you
+            the first words should be finded
+            Whatever hold you back
+            I can, I can get it off
+
+            Tell me what,
+            tell me what,
+            tell me what you want
+            I don't know why,
+            don't know why,
+            don't know why you afraid
+            Tell me what,
+            tell me what,
+            tell me what you say
+            I don't know why,
+            don't know why,
+            too late, it's too late
+
+            Have no fear for real,
+            it's just a turning wheel
+            Once you start up,
+            there's no other way
+            Don't put your eyes on boots,
+            step forward your roots
+            There's no aid, there's no trade
+            it belongs to you
+
+            Before you miss something given
+            You should know what's the truth
+            I can, I can make it out
+
+            Tell me what,
+            tell me what,
+            tell me what you want
+            I don't know why,
+            don't know why,
+            don't know why you afraid
+            Tell me what,
+            tell me what,
+            tell me what you say
+            I don't know why,
+            don't know why,
+            too late, it's too late
+
+            Before you miss something given
+            You should know what's the truth
+            I can, I can make it out
+
+            Tell me what,
+            tell me what,
+            tell me what you want
+            I don't know why,
+            don't know why,
+            don't know why you afraid
+            Tell me what,
+            tell me what,
+            tell me what you say
+            I don't know why,
+            don't know why,
+            too late, it's too late
+
+            Tell me what,
+            tell me what,
+            tell me what you want
+            I don't know why,
+            don't know why,
+            don't know why you afraid
+            Tell me what,
+            tell me what,
+            tell me what you say
+            I don't know why,
+            don't know why,
+            too late, it's too late`,
+        yt_link: "https://music.youtube.com/watch?v=9ssSxkyqaLI"
+    },
+
+    3: {
+        id: 3,
+        name: "lucy~",
+        creator: "Corbon Amodio",
+        genre: "Гітара",
+        count_listener: "3,1 млн.",
+        words: `
+            Lucy laid
+            On her back
+            While my head spun off the track
+            Laying still
+            On the grass
+            When did things move so damn fast?
+
+            Ooh, ah
+            Ooh, ah
+
+            Hearing the words
+            That I want
+            But was still caught so off guard
+            Sunny sky
+            Stiff smile
+            While the beats rise in my heart
+
+            Bingo, bingo, baby
+            I love you, ain't that crazy?
+            I want you, I want you over again
+            Bingo, bingo, baby
+            I love you, ain't that crazy?
+            I want you, I want you over again
+
+            Oh-oh, yeah-yeah
+            Oh-oh, yeah-yeah
+            Oh-oh, yeah-yeah
+            Oh-oh, yeah-yeah`,
+        yt_link: "https://music.youtube.com/watch?v=_2uTYUPe25E&list=LM"
+    },
+
+    4: {
+        id: 4,
+        name: "Спочатку було Слово",
+        creator: "МУР",
+        genre: "Гітара, фортепіано",
+        count_listener: "1,1 млн.",
+        words: "Ну чё те не хватает? Повітря, чистого повітря (Отче наш, що єси на небесах) (Нехай святиться ім'я Твоє, нехай прийде Царство Твоє) Тілом хай в землі, ім'ям не знищеним Вийти би живим, вийти би живим Відлунюю тобі голосом глухим Вийти би живим, вийти би живим Спочатку було Слово, і Слово було Бог Потім було декілька славетних перемог Що було жовто-блакитним, у червоне окропилось Бога відмінили — Слово залишилось Сначала было слово, и слово было (Ленин) Лєнін затискав народ, як надійний ремінь Тільки пояс або шию — хай кожен обирає Когось кладуть в вагони — хтось у них сідає Спочатку було слово, потім був будинок Майже ритуальний за кількістю поминок Майже ми не зникнули, майже ми живі Але на суді історії крісла всі пусті Тілом хай в землі, ім'ям не знищеним Вийти би живим, вийти би живим Чи над націєй моєю вже розтанув дим? Вийти би живим, вийти би живим! Спочатку було слово, один короткий підпис Байдужо-металевий чорнильной ручки відблиск Потім були люди, заковані у шкіру Люди, що накази не піддають сумніву Вони розрізають ніч ножами світла фар І жертиме усіх радянський цей дракар Спочатку було слово, потім стук у двері Розірвані обійми і знайдені папери Потім у дракона розігрався апетит Їжа ще смачніша, коли замість блюда щит Той дракон, він не жує — він заглотить і ковтне Ми зустрінемось з тобою там, де 20 тисяч льє Там, де Плужник і да Вінчі; там, де зло нас не знайде Там, де Ратушний і Курбас, — там ніхто не нападе Там, де буде Примаченко малювати Семенка Там, де цілі всі будинки і не знищені міста Там, де уряд УНР святкуватиме сто років Там, де голоду нема і сади замість окопів Там, де сонячний Тичина; там, де друзі всі живі Ми зустрінемось з тобою десь у твоїй голові Тілом хай в землі, ім'ям не знищеним Вийти би живим, вийти би живим Чи над націєй моєю вже розтанув дим? Вийти би живим, вийти би живим! (Ватуля, де вібрації?) Расстрелять Театр має бути, яким суспільство має бути завтра Расстрелять (струнко!) Ти що, не почув її? (Комих, Комих) Расстрелять (Лесику, це так поетично) Расстрелять (какая у вас фамилия?) Расстрелять Я на смерті боюся, мені віршів своїх шкода Расстрелять!",
+        yt_link: "https://music.youtube.com/watch?v=O0wlMCoAQNQ"
+    },
+
+};
+
+let object_len = Object.keys(song_db).length;
+document.getElementById('music_count').innerText = object_len;
+
+const clear_song_list = () => {
+    div_with_musics.innerText = "";
+    div_with_musics.insertAdjacentHTML('beforeend',
+        `<div style="margin-bottom: 8px; margin-top: 8px;">
+            <div class="id">№</div>
+            <div class="name">Назва</div>
+            <div class="creator">Виконавець</div>
+            <div class="genre">Жанри</div>
+            <div class="count_listener">К-сть слухачів</div>
+        </div>`
+    );
+};
+
+const show_all_songs = () => {
+    clear_song_list();
+    for (let i = 0; i < object_len; i++) {
+        div_with_musics.insertAdjacentHTML('beforeend',
+            `<div style="margin-bottom: 8px;">
+                    <div class="id">${song_db[i].id + 1}</div>
+                    <div class="name">${song_db[i].name}   |   <a target="_blank" href="${song_db[i].yt_link}">YT</a></div>
+                    <div class="creator">${song_db[i].creator}</div>
+                    <div class="genre">${song_db[i].genre}</div>
+                    <div class="count_listener">${song_db[i].count_listener}</div>
+                </div>`)
+    }
+};
+
+const div_with_musics = document.getElementById('music_table');
+show_all_songs();
+
+const search_by_name_input = document.getElementById('search_by_name_input');
+const search_by_genre_input = document.getElementById('search_by_genre_input');
+const search_by_words_input = document.getElementById('search_by_words_input');
+
+const search_by_name_button = document.getElementById('search_by_name_button');
+const search_by_genre_button = document.getElementById('search_by_genre_button');
+const search_by_words_button = document.getElementById('search_by_words_button');
+
+search_by_name_button.addEventListener('click', function () {
+    if (search_by_name_input.value != "") {
+        let found = false;
+        clear_song_list();
+
+        for (let i = 0; i < object_len; i++) {
+            if (song_db[i].name.toLowerCase().includes(search_by_name_input.value.toLowerCase())) {
+                console.log(`${search_by_name_input.value} - ${song_db[i].name}`);
+
+                div_with_musics.insertAdjacentHTML('beforeend',
+                    `<div style="margin-bottom: 8px;">
+                        <div class="id">${song_db[i].id + 1}</div>
+                        <div class="name">${song_db[i].name}   |   <a target="_blank" href="${song_db[i].yt_link}">YT</a></div>
+                        <div class="creator">${song_db[i].creator}</div>
+                        <div class="genre">${song_db[i].genre}</div>
+                        <div class="count_listener">${song_db[i].count_listener}</div>
+                    </div>`);
+
+                found = true;
+            };
+        };
+        if (!found) {
+            div_with_musics.insertAdjacentHTML("beforeend", `<div style="position:absolute; font-size:24px; left: calc(50% - 125px); top: calc(50% - 14px + 60px);">Не знайдено такої пісні.</div>`);
+        }
+    } else {
+        show_all_songs();
+    };
+});
+
+search_by_genre_button.addEventListener('click', function () {
+    if (search_by_genre_input.value != "") {
+        let found = false;
+        clear_song_list();
+
+        for (let i = 0; i < object_len; i++) {
+            if (song_db[i].genre.toLowerCase().includes(search_by_genre_input.value.toLowerCase())) {
+                console.log(`${search_by_genre_input.value} - ${song_db[i].genre}`);
+
+                div_with_musics.insertAdjacentHTML('beforeend',
+                    `<div style="margin-bottom: 8px;">
+                        <div class="id">${song_db[i].id + 1}</div>
+                        <div class="name">${song_db[i].name}   |   <a target="_blank" href="${song_db[i].yt_link}">YT</a></div>
+                        <div class="creator">${song_db[i].creator}</div>
+                        <div class="genre">${song_db[i].genre}</div>
+                        <div class="count_listener">${song_db[i].count_listener}</div>
+                    </div>`);
+
+                found = true;
+            };
+        };
+        if (!found) {
+            div_with_musics.insertAdjacentHTML("beforeend", `<div style="position:absolute; font-size:24px; left: calc(50% - 125px); top: calc(50% - 14px + 60px);">Не знайдено такої пісні.</div>`);
+        }
+    } else {
+        show_all_songs();
+    };
+});
+
+search_by_words_button.addEventListener('click', function () {
+    if (search_by_words_input.value != "") {
+        let found = false;
+        clear_song_list();
+
+        for (let i = 0; i < object_len; i++) {
+            if (song_db[i].words.toLowerCase().includes(search_by_words_input.value.toLowerCase())) {
+                console.log(`${search_by_words_input.value} - ${song_db[i].words}`);
+
+                div_with_musics.insertAdjacentHTML('beforeend',
+                    `<div style="margin-bottom: 8px;">
+                        <div class="id">${song_db[i].id + 1}</div>
+                        <div class="name">${song_db[i].name}   |   <a target="_blank" href="${song_db[i].yt_link}">YT</a></div>
+                        <div class="creator">${song_db[i].creator}</div>
+                        <div class="genre">${song_db[i].genre}</div>
+                        <div class="count_listener">${song_db[i].count_listener}</div>
+                    </div>`);
+
+                found = true;
+            };
+        };
+        if (!found) {
+            div_with_musics.insertAdjacentHTML("beforeend", `<div style="position:absolute; font-size:24px; left: calc(50% - 125px); top: calc(50% - 14px + 60px);">Не знайдено такої пісні.</div>`);
+        }
+    } else {
+        show_all_songs();
+    };
+});
